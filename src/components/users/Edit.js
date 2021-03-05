dddb45bc52bd88facc3659f3cc43d4914795e3b5
AndersonRochaIzap <dan.sjose20@gmail.com>
2021-03-05 19:21:14 -0300

    Correções busca na api

--- src/components/users/Edit.js
+++ src/components/users/Edit.js
@@ -1,51 +1,95 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import api from '../../services/api';
 
-export default class UserForm extends Component{
+export default class UserForm extends Component {
 
-    state = {
-        _user :{},
-    }
+  state = {
+    _user: {
+      ID: '',
+      USERNAME: '',
+      PASSWORD: '',
+      STATUS: ''
+    },
+  }
 
-async componentDidMount() {
-    console.log("teste")
+  async componentDidMount() {
     const { id } = this.props.match.params;
     const response = await api.get(`/users/${id}`);
-    this.setState({ _user : response.data });
-}
+    this.setState({ _user: response.data });
+  }
 
+  handleInputChange(event) {
+    this.setState({
+      ...this.state,
+      _user: {
+        ...this.state._user,
+        [event.target.name]: event.target.value
+      }
+    })
+  }
 
-    render() {
-        const { _user} = this.state;
-        return(
-            
-                <div className="col-sm-12">
-                    <form>
-                        <div className="form-group">
-                            <label for="exampleInputEmail1">ID</label>
-                            <input type="text" value={_user.ID} className="form-control" id="id" aria-describedby="emailHelp" placeholder="Seu email" />
-                            <small id="emailHelp" className="form-text text-muted"></small>
-                        </div>
-                        <div className="form-group">
-                            <label for="exampleInputPassword1">User Name</label>
-                            <input type="text" value={_user.USERNAME} className="form-control" id="userneme" placeholder="Senha" />
-                        </div>
-                        <div className="form-group">
-                            <label for="exampleInputPassword1">Password</label>
-                            <input type="text" value={_user.PASSWORD} className="form-control" id="password" placeholder="Senha" />
-                        </div>
-                        
-                        <div className="form-group">
-                            <label for="exampleInputPassword1">Status</label>
-                            <input type="text" value={_user.STATUS} className="form-control" id="status" placeholder="Senha" />
-                        </div>
-            
-                        <button type="submit" className="btn btn-primary">Enviar</button>
-                      
-                    </form>
-                </div>
-        )
-    }
-    
+  render() {
+    const { _user } = this.state;
+
+    return (
+      <div className="col-sm-12">
+        <form>
+          <div className="form-group">
+            <label htmlFor="exampleInputEmail1">ID</label>
+            <input
+              value={_user.ID}
+              name="ID"
+              className="form-control"
+              id="ID"
+              placeholder="Seu email"
+              onChange={this.handleInputChange.bind(this)}
+            />
+            <small id="emailHelp" className="form-text text-muted"></small>
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="exampleInputPassword1">Username</label>
+            <input
+              value={_user.USERNAME}
+              className="form-control"
+              id="username"
+              name="USERNAME"
+              placeholder="Username"
+              onChange={this.handleInputChange.bind(this)}
+            />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="exampleInputPassword1">Password</label>
+            <input
+              value={_user.PASSWORD}
+              className="form-control"
+              id="password"
+              name="PASSWORD"
+              placeholder="Senha"
+              onChange={this.handleInputChange.bind(this)}
+            />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="exampleInputPassword1">Status</label>
+            <input
+              value={_user.STATUS}
+              className="form-control"
+              id="status"
+              name="STATUS"
+              placeholder="Status"
+              onChange={this.handleInputChange.bind(this)}
+            />
+          </div>
+
+          <button type="submit" className="btn btn-primary">Enviar</button>
+          <Link to={'/users'} className="btn btn-default"> Voltar </Link>
+        </form>
+      </div>
+    )
+  }
+
 }
 
--- src/components/users/New.js
+++ src/components/users/New.js
@@ -1,61 +1,61 @@
 import React, { Component } from 'react';
 import api from '../../services/api';
 
-export default class UserFormNew extends Component{
+export default class UserFormNew extends Component {
 
-    state = {
-        ID : '',
-        USERNAME : '',
-        PASSWORD : '',
-        STATUS : '',
-    }
+  state = {
+    ID: '',
+    USERNAME: '',
+    PASSWORD: '',
+    STATUS: '',
+  }
 
-    dataChange(ev){
-        this.setState({
-            [ev.target.name] : ev.target.value
-        })
-    }
+  dataChange(ev) {
+    this.setState({
+      [ev.target.name]: ev.target.value
+    })
+  }
 
-    handleSubmit = event => {
-        event.preventDefaut();
-        api.post('/users', this.state)
-        .then(res => {
-            console.log(res);
-        })
-    }
+  handleSubmit = event => {
+    event.preventDefaut();
+    api.post('/users', this.state)
+      .then(res => {
+        console.log(res);
+      })
+  }
 
 
 
-    render() {
-        return(
-            
-                <div className="col-sm-12">
-                    <form onSubmit={this.handleSubmit}>
-                        <div className="form-group">
-                            <label for="exampleInputEmail1">ID</label>
-                            <input type="text" name="ID" value={this.state.ID} onChange={this.dataChange.bind(this)} className="form-control" aria-describedby="emailHelp" placeholder="Seu email" />
-                            <small id="emailHelp" className="form-text text-muted"></small>
-                        </div>
-                        <div className="form-group">
-                            <label for="exampleInputPassword1">User Name</label>
-                            <input type="text" name="USERNAME" value={this.state.USERNAME} onChange={this.dataChange.bind(this)} className="form-control" placeholder="Senha" />
-                        </div>
-                        <div className="form-group">
-                            <label for="exampleInputPassword1">Password</label>
-                            <input type="text" name="PASSWORD" value={this.state.PASSWORD} onChange={this.dataChange.bind(this)} className="form-control" placeholder="Senha" />
-                        </div>
-                        
-                        <div className="form-group">
-                            <label for="exampleInputPassword1">Status</label>
-                            <input type="text" name="STATUS" value={this.state.STATUS} onChange={this.dataChange.bind(this)} className="form-control"  placeholder="Senha" />
-                        </div>
-            
-                        <button type="submit" className="btn btn-primary">Enviar</button>
-                        <button type="submit" className="btn btn-primary" to={`/users`}>Lista</button>
-                    </form>
-                </div>
-        )
-    }
-    
+  render() {
+    return (
+
+      <div className="col-sm-12">
+        <form onSubmit={this.handleSubmit}>
+          <div className="form-group">
+            <label for="exampleInputEmail1">ID</label>
+            <input type="text" name="ID" value={this.state.ID} onChange={this.dataChange.bind(this)} className="form-control" aria-describedby="emailHelp" placeholder="Seu email" />
+            <small id="emailHelp" className="form-text text-muted"></small>
+          </div>
+          <div className="form-group">
+            <label for="exampleInputPassword1">User Name</label>
+            <input type="text" name="USERNAME" value={this.state.USERNAME} onChange={this.dataChange.bind(this)} className="form-control" placeholder="Senha" />
+          </div>
+          <div className="form-group">
+            <label for="exampleInputPassword1">Password</label>
+            <input type="text" name="PASSWORD" value={this.state.PASSWORD} onChange={this.dataChange.bind(this)} className="form-control" placeholder="Senha" />
+          </div>
+
+          <div className="form-group">
+            <label for="exampleInputPassword1">Status</label>
+            <input type="text" name="STATUS" value={this.state.STATUS} onChange={this.dataChange.bind(this)} className="form-control" placeholder="Senha" />
+          </div>
+
+          <button type="submit" className="btn btn-primary">Enviar</button>
+          <button type="submit" className="btn btn-primary" to={`/users`}>Lista</button>
+        </form>
+      </div>
+    )
+  }
+
 }
 
